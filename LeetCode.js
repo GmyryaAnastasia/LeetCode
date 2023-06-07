ab1ef0e3fe9eb0908a8f47f4b2d7787517fc0f0f
GmyryaAnastasia <nastgm26@gmail.com>
2023-06-07 15:53:54 +0300

    added task-2

--- LeetCode.js
+++ LeetCode.js
@@ -8,3 +8,30 @@ var buyChoco = function(prices, money) {
     }
 
 };
+
+// task-2
+var romanToInt = function(s) {
+    const romanToDecimalMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let decimal = 0;
+    for (let i = 0; i < s.length; i++) {
+        const current=romanToDecimalMap[s.charAt(i)]
+        const next=romanToDecimalMap[s.charAt(i+1)]
+        if(next>current){
+            decimal+= next-current
+            i++
+        }else {
+            decimal+= current
+        }
+    }
+
+    return decimal;
+};
